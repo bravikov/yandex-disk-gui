a8036af15760b6d7fd04da2d6c45fe1821a51f35
Dmitry Bravikov <dmitry@bravikov.pro>
2019-02-03 12:12:24 +0300

    Add application version and about message

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,15 @@
 #include <QSystemTrayIcon>
 #include <QMenu>
 #include <QObject>
+#include <QMessageBox>
 
-void showStatus();
+void about();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName("Yandex Disk GUI");
+    a.setApplicationVersion("0.1.0");
     a.setQuitOnLastWindowClosed(false);
 
     YandexDisk yandexDisk;
@@ -23,9 +26,28 @@ int main(int argc, char *argv[])
     menu.addAction(QObject::tr("Stop"), &yandexDisk, &YandexDisk::stop);
     menu.addSeparator();
     menu.addAction(QObject::tr("Exit"), &a, &QApplication::exit);
+    menu.addSeparator();
+    menu.addAction(QObject::tr("About"), &a, &about);
 
     tray.setContextMenu(&menu);
     tray.show();
 
     return a.exec();
+}
+
+void about()
+{
+    const QString name{QApplication::applicationName()};
+    const QString version{QApplication::applicationVersion()};
+    const QString url{"https://github.com/bravikov/yandex-disk-gui"};
+    const QString email{"dmitry@bravikov.pro"};
+    const QString message{
+        QObject::tr(
+            "<h1>%1</h1>"
+            "Version: %2."
+            "<p>Open source <a href='%3'>project</a>.</p>"
+            "<p>© 2019 Dmitry Bravikov (<a href='mailto:%4'>%4</a>)</p>"
+        ).arg(name).arg(version).arg(url).arg(email)
+    };
+    QMessageBox::about(nullptr, name, message);
 }
